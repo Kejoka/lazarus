63d512772bc3cbdf2e915f91bf252341482c416c
Palma Frittmann <palma.frittmann@stud.htwk-leipzig.de>
2023-03-09 16:21:36 +0100

    Dodo AI working

---
+++ Assets/Scripts/DodoAI.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DodoAI : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private NavMeshAgent navAgent;
+
+    [SerializeField]
+    private float range; //range of movement (sphere radius) from centerpoint
+
+    [SerializeField]
+    private Transform centrePoint;
+
+    private bool stoppedWalking = false;
+
+    void Start()
+    {
+        navAgent.SetDestination(centrePoint.position);
+        animator.SetTrigger("StartWalking");
+    }
+
+    void Update()
+    {
+        if (navAgent.remainingDistance <= 0.1) //done with path 
+        {
+            if (!stoppedWalking)
+            {
+                animator.SetTrigger("StoppedWalking");
+                stoppedWalking = true;
+            }
+
+            if (animator.GetBool("IdleFinished"))
+            {
+                Vector3 point;
+                if (RandomPoint(centrePoint.position, range, out point)) 
+                {
+                    animator.SetTrigger("StartWalking");
+                    stoppedWalking = false;
+                    navAgent.SetDestination(point);
+                    animator.SetBool("IdleFinished", false);
+                }
+            }
+
+        }
+    }
+
+    //Generate RandomPoint in a sphere
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //check if randomPoint lies within navmesh
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+}
--- Assets/Scripts/MammothAI.cs
+++ Assets/Scripts/MammothAI.cs
@@ -8,9 +8,6 @@ public class MammothAI : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
-    AnimatorClipInfo[] animatorinfo;
-    string current_animation;
-
     [SerializeField]
     private NavMeshAgent navAgent;
 
@@ -21,49 +18,17 @@ public class MammothAI : MonoBehaviour
 
     private Vector3 destination;
 
-    private float range = 30.0f; //radius of sphere
-
-    [SerializeField]
-    private Transform centrePoint;
-
     private bool stoppedWalking = false;
 
     void Start()
     {
-        navAgent.SetDestination(centrePoint.position);
         animator.SetTrigger("StartWalking");
-        //UpdateDestination();
-
+        UpdateDestination();
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        
-        if (navAgent.remainingDistance <= 1.4) //done with path //navAgent.stoppingDistance
-        {
-            if (!stoppedWalking)
-            {
-                animator.SetTrigger("StoppedWalking");
-                stoppedWalking = true;
-            }
-            
-            if (animator.GetBool("IdleFinished"))
-            {
-                Vector3 point;
-                if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
-                {
-                    animator.SetTrigger("StartWalking");
-                    stoppedWalking = false;
-                    navAgent.SetDestination(point);
-                    animator.SetBool("IdleFinished", false);
-                }
-            }
-            
-        }
-        
-        /*
-       if(Vector3.Distance(transform.position, destination) < 1.0f) //done with path
+    {      
+       if(Vector3.Distance(transform.position, destination) <= 1.4) //done with path
         {
             if (!stoppedWalking)
             {
@@ -74,20 +39,21 @@ public class MammothAI : MonoBehaviour
             if (animator.GetBool("IdleFinished"))
             {
                 animator.SetBool("IdleFinished", false);
+                animator.SetTrigger("StartWalking");
+                stoppedWalking = false;
                 UpdateDestination();
             }
 
         }
-        */
+        
     }
 
     private void UpdateDestination()
     {
         destination = wayPoints[wayPointIndex].position;
         navAgent.SetDestination(destination);
-        animator.SetTrigger("StartWalking");
 
-        if(wayPointIndex == wayPoints.Length-1)
+        if (wayPointIndex == wayPoints.Length-1)
         {
             wayPointIndex = 0;
         }
@@ -97,20 +63,5 @@ public class MammothAI : MonoBehaviour
         }
     }
 
-    bool RandomPoint(Vector3 center, float range, out Vector3 result)
-    {
-
-        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
-        NavMeshHit hit;
-        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
-        {
-            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
-            //or add a for loop like in the documentation
-            result = hit.position;
-            return true;
-        }
-
-        result = Vector3.zero;
-        return false;
-    }
+    
 }
