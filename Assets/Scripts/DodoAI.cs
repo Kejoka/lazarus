870d5672da2b0274bd1b70f2da8ba1b91ff8820c
kkaubuko <kenn.kaubukowski@stud.htwk-leipzig.de>
2023-03-10 21:41:14 +0100

    manno

---
+++ Assets/Scripts/DodoAI.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DodoAI : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private NavMeshAgent navAgent;
+
+    [SerializeField]
+    private float range; //range of movement (sphere radius) from centerpoint
+
+    [SerializeField]
+    private Transform centrePoint;
+
+    [SerializeField]
+    private AudioSource dodoSound;
+
+    private bool stoppedWalking = false;
+
+    void Start()
+    {
+        navAgent.SetDestination(centrePoint.position);
+        animator.SetTrigger("StartWalking");
+    }
+
+    void Update()
+    {
+        if (navAgent.remainingDistance <= 0.1) //done with path 
+        {
+            if (!stoppedWalking)
+            {
+                animator.SetTrigger("StoppedWalking");
+                stoppedWalking = true;
+                dodoSound.Play();
+            }
+
+            if (animator.GetBool("IdleFinished"))
+            {
+                Debug.Log("Looking for new point..");
+                Vector3 point;
+                if (RandomPoint(centrePoint.position, range, out point)) 
+                {
+                    Debug.Log("Found one: " + point);
+                    animator.SetTrigger("StartWalking");
+                    stoppedWalking = false;
+                    navAgent.SetDestination(point);
+                    animator.SetBool("IdleFinished", false);
+                }
+            }
+
+        }
+    }
+
+    //Generate RandomPoint in a sphere
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //check if randomPoint lies within navmesh
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+}
---
+++ Assets/Scripts/MammothAI.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MammothAI : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource mammothTrumpet;
+
+    [SerializeField]
+    private AudioSource mammothGrowl;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private NavMeshAgent navAgent;
+
+    [SerializeField]
+    private Transform[] wayPoints;
+
+    private int wayPointIndex = 0;
+
+    private Vector3 destination;
+
+    private bool stoppedWalking = false;
+
+    void Start()
+    {
+        animator.SetTrigger("StartWalking");
+        UpdateDestination();
+    }
+
+    void Update()
+    {      
+       if(Vector3.Distance(transform.position, destination) <= 2.1) //done with path
+        {
+            if (!stoppedWalking)
+            {
+                animator.SetTrigger("StoppedWalking");
+                stoppedWalking = true;
+            }
+
+            if (animator.GetBool("TrunkStarted"))
+            {
+                mammothTrumpet.Play();
+                animator.SetBool("TrunkStarted", false);
+            }
+
+            if (animator.GetBool("IdleStarted"))
+            {
+                mammothGrowl.Play();
+                animator.SetBool("IdleStarted", false);
+            }
+
+            if (animator.GetBool("IdleFinished"))
+            {
+                animator.SetBool("IdleFinished", false);
+                animator.SetTrigger("StartWalking");
+                stoppedWalking = false;
+                UpdateDestination();
+            }
+
+        }
+        
+    }
+
+    private void UpdateDestination()
+    {
+        destination = wayPoints[wayPointIndex].position;
+        navAgent.SetDestination(destination);
+
+        if (wayPointIndex == wayPoints.Length-1)
+        {
+            wayPointIndex = 0;
+        }
+        else
+        {
+            wayPointIndex++;
+        }
+    }
+
+    
+}
